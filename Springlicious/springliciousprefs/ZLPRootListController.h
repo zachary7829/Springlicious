76ef03574287342a781e935c49b6f74c2f29027e
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-03-10 14:53:04 -0500

    flesh out dock color options

---
+++ Springlicious/springliciousprefs/ZLPRootListController.h
@@ -0,0 +1,12 @@
+#import <Preferences/PSListController.h>
+#import <spawn.h>
+
+@interface NSTask : NSObject
+-(void)setLaunchPath:(id)arg1;
+-(void)setArguments:(id)arg1;
+-(void)launch;
+@end
+
+@interface ZLPRootListController : PSListController
+
+@end
